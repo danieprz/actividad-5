ac29e121fc2a54d92076050ceb3634c57b1620b8
Pérez <daniellopezperez2005@gmail.com>
2023-04-19 13:34:31 +0200

    all

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola, introduzca su nombre a continuación por favor.");
+var nombre = Console.ReadLine();
+Console.Write("Un placer,");
+for (int i = (nombre.Length-1); i >= 0; i--)
+{
+    Console.Write(nombre[i]);
+}
